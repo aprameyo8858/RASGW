5d2e92334082e433f1379c327b4c9c5b74c6b58d
aprameyo8858 <146671193+aprameyo8858@users.noreply.github.com>
2025-01-09 02:45:15 +0530

    Update rasgw_gan.py

--- rasgw_gan.py
+++ rasgw_gan.py
@@ -191,7 +191,7 @@ class Target_model(nn.Module):
         x = F.relu(x)
         x = self.fc3(x)
         x = torch.cat((x_init,x),dim=1)
-        x = F.relu(x)        # was commented out originally
+        #x = F.relu(x)        # was commented out originally
         return x.to('cuda')
     def forward_remaining(self,x):
         #x = F.relu(self.fc1(x))            #was commented out originally
